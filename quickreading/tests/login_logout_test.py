7ebb0729c1fb1e8348c313775958a02a60f487f8
Yan Liu <278899085@qq.com>
2019-04-24 22:27:47 +0100

    add unitTest: user register,login,logout,search novel

---
+++ quickreading/tests/login_logout_test.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+
+
+class UserLogin(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_user_login_logout(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8001/")
+        # General user
+        driver.find_element_by_link_text("Login").click()
+        driver.find_element_by_id("user_name").click()
+        driver.find_element_by_id("user_name").clear()
+        driver.find_element_by_id("user_name").send_keys("test5")
+        driver.find_element_by_id("user_password").click()
+        driver.find_element_by_id("user_password").clear()
+        driver.find_element_by_id("user_password").send_keys("123456")
+        driver.find_element_by_id("user_login").click()
+        driver.find_element_by_link_text("test5").click()
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='Payment'])[1]/following::span[2]").click()
+
+        # VIP user
+        driver.find_element_by_link_text("Login").click()
+        driver.find_element_by_id("user_name").click()
+        driver.find_element_by_id("user_name").clear()
+        driver.find_element_by_id("user_name").send_keys("test1")
+        driver.find_element_by_id("user_password").click()
+        driver.find_element_by_id("user_password").clear()
+        driver.find_element_by_id("user_password").send_keys("123456")
+        driver.find_element_by_id("user_login").click()
+        driver.find_element_by_link_text("test1").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Renew VIP'])[1]/following::span[2]").click()
+
+        # admin login logout
+        driver.find_element_by_link_text("Login").click()
+        driver.find_element_by_id("user_name").click()
+        driver.find_element_by_id("user_name").clear()
+        driver.find_element_by_id("user_name").send_keys("AdminYanLiu")
+        driver.find_element_by_id("user_password").click()
+        driver.find_element_by_id("user_password").clear()
+        driver.find_element_by_id("user_password").send_keys("LY19961222..")
+        driver.find_element_by_id("user_login").click()
+        driver.find_element_by_link_text("AdminYanLiu").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Payment'])[1]/following::span[2]").click()
+        driver.close()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ quickreading/tests/search_novels_test.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(60)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("http://0.0.0.0:8001/")
+        driver.find_element_by_name("wd").click()
+        driver.find_element_by_name("wd").clear()
+        driver.find_element_by_name("wd").send_keys(u"重燃")
+        driver.find_element_by_id("searchbtn").click()
+        driver.find_element_by_link_text(u"重燃最新章节列表_重燃全文阅读- 天籁小说").click()
+        driver.find_element_by_link_text(u"第二章 不一样的世界").click()
+        sleep(3)
+        driver.close()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
